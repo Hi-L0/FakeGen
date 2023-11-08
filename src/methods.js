c2b0ddcb492aa711bf532ed581cb3e0282414dfe
hilo <7amzalaaroussi@gmail.com>
2023-11-08 15:35:05 +0100

    add some annotations to the methods

--- src/methods.js
+++ src/methods.js
@@ -1,8 +1,11 @@
-
+//@param int range
+//random int with the range
 function randomInt(range) {
     return Math.floor(Math.random() * range);
   }
-
+  
+//@param array[]
+//get random value in the array
 function randomValueInArray(arr) {
     return arr[randomInt(arr.length)];
 }
