1329f5e226ce989ed8b759142e8e94ffcf30f744
hilo <7amzalaaroussi@gmail.com>
2023-11-15 22:22:31 +0100

    adding new methods

--- src/methods.js
+++ src/methods.js
@@ -10,4 +10,34 @@ function randomValueInArray(arr) {
     return arr[randomInt(arr.length)];
 }
 
-module.exports= randomValueInArray
+function multiplValueArr(size,array){
+  if (size != null) {
+    const vals = [];
+    var i = 0;
+    while (i < size) {
+      vals.push(randomValueInArray(array));
+      i++;
+    }
+    return vals;
+  }else{
+    return randomValueInArray(array);
+  }
+}
+function randomAdress(street,city,country,zip){
+  var address_country=country;
+  var street=street
+  var address_city=city
+  var code=zip
+  var address= `${street} ${address_city} ${address_country} ${code}`  
+  return address;
+}
+
+function randomLocation(city,country,zip){
+  return {
+    city:city,
+    country:country,
+    Zip:zip,
+  }
+}
+
+module.exports= {randomValueInArray,multiplValueArr,randomAdress,randomLocation}
