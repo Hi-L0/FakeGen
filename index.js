e0fbf45fecc3477c6c03540ac1bb3cdefb25693f
hilo <7amzalaaroussi@gmail.com>
2023-11-15 22:24:03 +0100

    edit packages

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const fakeGen = require("./src/generate");
 const fakeGenAR = require("./src/generateAR");
 
-var value = {
-  user: {
-    // id: fakeGen.id(),
-    name: fakeGen.generateFirstName(),
-    // lastname: fakeGenAR.generateLastName(),
-    // fullname: fakeGen.FullName(),
-    // city: fakeGen.generateCity(),
-    // adress: fakeGen.generateAddress(),
-    // city: fakeGenAR.cityMR(),
-    products:fakeGen.products(1),  // number as params
-    // date:fakeGen.betweenDates()
-  },
-  // size:AR_names
-};
+console.log(fakeGenAR.company())
 
-console.log(value);
+
+module.exports={
+  fakeGen,
+  fakeGenAR,
+}
